865f59fe0c3a88b5c8fa66f4f83acb2f25386e77
Steve P <spdevuk@gmail.com>
2020-01-22 21:44:21 +0000

    feat: add validations for user signup

---
+++ app/Validators/CreateUser.js
@@ -0,0 +1,29 @@
+'use strict'
+
+class CreateUser {
+  get rules () {
+    return {
+      username: 'required|unique:users',
+      email: 'required|unique:users',
+      password: 'required'
+    }
+  }
+
+  get messages () {
+    return {
+      required: '{{ field }} is required.',
+      unique: '{{ field }} already exists'
+    }
+  }
+
+  async fails (error) {
+    this.ctx.session.withErrors(error)
+      .flashAll()
+
+    return this.ctx.response.redirect('back')
+  }
+}
+
+module.exports = CreateUser
+
+module.exports = CreateUser
