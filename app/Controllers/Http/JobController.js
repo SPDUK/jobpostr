adbc4ea7b1bdaf908a62b2322ba4a332f892683e
Steve P <spdevuk@gmail.com>
2020-01-22 21:24:21 +0000

    feat: add job model & display them

---
+++ app/Controllers/Http/JobController.js
@@ -0,0 +1,106 @@
+'use strict'
+
+const Job = use('App/Models/Job')
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+/**
+ * Resourceful controller for interacting with jobs
+ */
+class JobController {
+  /**
+   * Show a list of all jobs.
+   * GET jobs
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async home ({ request, response, view }) {
+    // Create a job
+    // const job = new Job()
+    // job.title = 'My job title'
+    // job.link = 'http://google.com'
+    // job.description = 'My job description'
+    // await job.save()
+
+    // Fetch a job
+    const jobs = await Job.all()
+
+    return view.render('index', { jobs: jobs.toJSON() })
+  }
+
+  /**
+   * Render a form to be used for creating a new job.
+   * GET jobs/create
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async create ({ request, response, view }) {
+  }
+
+  /**
+   * Create/save a new job.
+   * POST jobs
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store ({ request, response }) {
+  }
+
+  /**
+   * Display a single job.
+   * GET jobs/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async show ({ params, request, response, view }) {
+  }
+
+  /**
+   * Render a form to update an existing job.
+   * GET jobs/:id/edit
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async edit ({ params, request, response, view }) {
+  }
+
+  /**
+   * Update job details.
+   * PUT or PATCH jobs/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async update ({ params, request, response }) {
+  }
+
+  /**
+   * Delete a job with id.
+   * DELETE jobs/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async destroy ({ params, request, response }) {
+  }
+}
+
+module.exports = JobController
